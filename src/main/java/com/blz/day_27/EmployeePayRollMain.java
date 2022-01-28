7221024abf30bed7f89b21aba75423f1d7f9c482
sk09822 <kanadesandipan.virtuoso@gmail.com>
2022-01-28 15:05:44 +0530

    Employee Payroll Service

---
+++ src/main/java/com/blz/day_27/EmployeePayRollMain.java
@@ -0,0 +1,7 @@
+package com.blz.day_27;
+
+public class EmployeePayRollMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee payroll service");
+    }
+}
